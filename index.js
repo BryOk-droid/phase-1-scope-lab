853927f9e6d1fea37be1c07c2a282775e46615b0
BryOk-droid <brianokoth534@gmail.com>
2025-03-06 11:20:22 +0300

    Implemented scope manipulation functions for customer data management

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+// Declare a variable in global scope called customerName
+var customerName = "bob";
+
+// Function to uppercase the global customerName variable
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Function to set bestCustomer in global scope
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+// Function to overwrite bestCustomer variable
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+// Declare a constant in global scope
+const leastFavoriteCustomer = "some customer";
+
+// Function to attempt changing the leastFavoriteCustomer constant
+function changeLeastFavoriteCustomer() {
+  throw new Error("Assignment to constant variable.");
+}
